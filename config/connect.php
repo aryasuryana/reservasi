06c4ebcf820fb5d93a35e9ba96c357552e1dc252
Reja Revaldy F <rejarevaldy30@gmail.com>
2022-03-01 22:06:19 +0800

    Update connect.php

--- config/connect.php
+++ config/connect.php
@@ -2,10 +2,6 @@
 
     $db = mysqli_connect('localhost', 'root', '', 'db_rent');
 
-    if ($db->connect_error) {
-      die("Connection failed: " . $db->connect_error);
-    }
-    echo "Connected successfully";
 
     function querySql($query)
         {
